8f27ddbd8862da21aaa9dda802342c9589dd4238
Kshitij Gotiwale <kgotiwale@gmail.com>
2022-02-21 00:54:12 -0600

    Navigation Controller - Kshitij

---
+++ UIAccess/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  UIAccess
+//
+//  Created by Kshitij Gotiwale on 2/21/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+}
